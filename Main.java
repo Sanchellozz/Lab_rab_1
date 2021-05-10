fe53db261da204c490c5e761452464e09e58226c
Sanchellozz <68155872+Sanchellozz@users.noreply.github.com>
2021-05-10 23:43:32 +0300

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
+        int[] f = new int[n+1];
+        f[0] = 0;
+        f[1] = 1;
+        for (int i = 2; i < n; ++i) {
+            f[i] = f[i - 1] + f[i - 2];
+        }
+        for (int i = 2; i < n; ++i) {
+            if (f[i] % 2 == 0) {
+                System.out.println(f[i]);
+            }
+        }
+    }
+}
